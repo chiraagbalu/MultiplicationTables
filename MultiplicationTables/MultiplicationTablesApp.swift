52235d48c64f827db4fd84c5180c57d2003aee95
Chiraag Balu <chiraag0729@outlook.com>
2020-11-03 16:12:54 -0800

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Chiraag Balu on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
